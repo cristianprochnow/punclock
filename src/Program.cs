6e7fb75bb5d9c83ffe8ab2a82a94460325427904
Cristian Prochnow <cristianprochnow@gmail.com>
2022-11-12 12:57:13 -0300

    feat: add frame from homepage

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,6 @@
 ﻿Screen screen = new Screen();
 
+// Constrói uma janela que preenche o espaço inteiro disponível.
+screen.buildFrame(0, 0, screen.WindowWidth, screen.WindowHeight);
+
 Console.ReadKey();
--- src/class/Screen.cs
+++ src/class/Screen.cs
@@ -1,22 +1,68 @@
-class Screen {
+class Screen
+{
   private ConsoleColor background;
   private ConsoleColor color;
+  private int sizeToAvoidError = 1;
+  public int WindowWidth
+  {
+    get {
+      return Console.WindowWidth - this.sizeToAvoidError;
+    }
+  }
+  public int WindowHeight
+  {
+    get {
+      return Console.WindowHeight - this.sizeToAvoidError;
+    }
+  }
 
-  public Screen() {
+  public Screen()
+  {
     this.background = ConsoleColor.Black;
     this.color = ConsoleColor.White;
 
     this.setUpScreen();
   }
 
-  private void setUpScreen() {
+  private void setUpScreen()
+  {
     Console.BackgroundColor = this.background;
     Console.ForegroundColor = this.color;
 
     Console.Clear();
   }
 
-  public void buildFrame(int initialColumn, int initialRow, int finalColumn, int finalRow) {
+  public void buildFrame(int initialColumn, int initialRow, int finalColumn, int finalRow)
+  {
+    // Desenha a linha superior;
+    for (int column = initialColumn; column < finalColumn; column++)
+    {
+      this.write(column, initialRow, "-");
+    }
+    // Desenha a linha de baixo que fecha o molde.
+    for (int column = initialColumn; column < finalColumn; column++)
+    {
+      /*
+        `-1` apenas para corrigir o fechamento da moldura, que estava ficando
+        uma linha para baixo do limite da lateral.
+      */
+      this.write(column, finalRow - 1, "-");
+    }
+    // Desenha a linha da esquerda.
+    for (int row = initialRow; row < finalRow; row++)
+    {
+      this.write(initialColumn, row, "|");
+    }
+    // Desenha a linha da direita.
+    for (int row = initialRow; row < finalRow; row++)
+    {
+      this.write(finalColumn, row, "|");
+    }
+  }
 
+  public void write(int column, int row, string content)
+  {
+    Console.SetCursorPosition(column, row);
+    Console.Write(content);
   }
 }
